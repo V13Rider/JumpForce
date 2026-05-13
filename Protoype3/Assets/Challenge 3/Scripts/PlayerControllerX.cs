934a631579a64d3c36147f4e78e31962e5069618
Oliver Abernethy <abernethyo@roverkids.org>
2026-05-13 14:19:57 -0400

    Chanllange ALMOST DONE

--- Protoype3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Protoype3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -16,13 +16,15 @@ public class PlayerControllerX : MonoBehaviour
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
-
-
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
+
+        playerRb = GetComponent<Rigidbody>();
 
         // Apply a small upward force at the start of the game
         playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
@@ -33,9 +35,9 @@ public class PlayerControllerX : MonoBehaviour
     void Update()
     {
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (Input.GetKeyDown(KeyCode.Space) && !gameOver)
         {
-            playerRb.AddForce(Vector3.up * floatForce);
+             playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
         }
     }
 
